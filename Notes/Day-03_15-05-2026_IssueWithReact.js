f411bfcf501c570441ba274d6d9b7cf1880bf23e
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-05-23 00:38:31 +0530

    Document issues with React and setup environment
    Added notes on issues with React and setup instructions for a development environment.

---
+++ Notes/Day-03_15-05-2026_IssueWithReact.js
@@ -0,0 +1,129 @@
+Issues with React [Concerns]
+- It is not designed for your
+requirements hence lot of GAP’s.
+- It requires lot of 3rd party plugins.
+- Virtual DOM
+- Pace of development
+- Poor documentation
+
+Setup Environment for React
+
+1. Download and Install “Node JS” on
+your PC.
+
+https://nodejs.org/
+
+- Node JS is a Javascript runtime.
+- It is free and open source runtime
+used by developers to build web apps,
+servers, command line tools &
+
+scripts [server scripts].
+
+- Mac Users select “.pkg”
+- Windows User select “.msi”
+
+2. Open command prompt in
+windows / terminal in Mac
+
+> node -v 20+. Version
+> npm -v 10+ version
+
+- NPM is a package manager.
+- Package manager is a tool used
+
+to install, update and uninstall any
+library
+
+required for project.
+
+a) Yarn
+b) Bower
+c) Composer
+d) NuGet
+e) RubyGems
+f) NPM
+
+3. Download & Install “Visual Studio
+Code Editor”.
+
+https://code.visualstudio.com/download
+
+- It provides an IDE for building,
+debugging, testing and deploying
+applications
+
+4. Open Visual Studio Code and
+Install following extensions
+
+a) Live Server [Ritwick Dey]
+b) vscode-icons [Microsoft]
+c) IntelliSense for CSS class
+
+names in HTML
+
+d) GitHub Copilot
+e) EsLint
+
+Setup Development Environment
+for Web Application:
+
+1. Create a new folder for project on
+your PC [any location]
+
+d:\web-app
+
+2. Open folder in VS code
+
+3. Open terminal in VS code [Ctrl +
+`] [Terminal Menu => New Terminal]
+
+4. Switch your terminal from PS
+[Power Shell] to Command Prompt
+
+5. Run the following command
+
+> npm init -y
+
+- It generates “package.json” file.
+- It comprises of project meta
+
+data.
+
+6. Add following files and folders
+into project
+
+Web-App
+|
+|_ package.json
+|_ .gitignore
+|_ README.md
+|_ public [folder]
+|_ src [folder]
+|_ index.html
+
+.gitignore It is used to
+ignore resources while publishing on
+to GIT.
+
+README.md It is a help
+
+document by developer for developer.
+
+public It comprises of
+all static resources like, html, images,
+videos,
+
+documents, .json
+
+files etc.
+
+src It comprises of
+all dynamic resources like, css, scss,
+.js, .jsx, .ts
+
+.tsx etc.
+
+index.html It is startup
+
+page for application.
