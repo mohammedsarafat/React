223c3ab42d8bc7b4898fe407fd5164191d7feba6
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-05-23 00:29:08 +0530

    Add introduction content for React JS
    Updated definitions and clarifications about React JS, its usage, and comparison with Angular.

---
+++ Notes/React-Day-1_13-05-2026_Introduction.js
@@ -0,0 +1,22 @@
+1. What is React JS?
+		- It is a Javascript library used to build UI for web.  [Old]
+		- It is a Javascript library for Web and Native UI.    [New]
+
+	2. Can we build an app using React?
+		- No. You can build UI for app.
+		- To build application you need support of framework like “Next.JS” 
+
+	3. What is difference between React & Angular?
+		- React is library. [build UI]
+		- Angular is a framework. [build apps]
+
+	4. What is difference between React & React JS?
+		- Both are same.
+		- But Angular & Angular JS are different from each other. 
+
+	5. Why we need technologies like React or Angular?
+		- For building SPA
+	
+	6. What are the challenges in modern web development?
+	
+	7. Features of React?
