e91e42d811c6baca69383d1dd33f5762fb86bc8c
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-05-23 00:36:29 +0530

    Add FAQ on modern application challenges and React features
    Added FAQ section discussing challenges in modern applications and solutions for building single-page applications, along with features of React JS.

---
+++ Notes/Day-02_14-05-2026_Introduction02.js
@@ -0,0 +1,43 @@
+FAQ
+---
+1.What are the challenges In Modern Application?
+-Application must have same experience across devices.
+-Mobile user must get access to everything.
+
+2.Fluid UX
+-User stays on one page and can get access to everything from single page.
+-Lot of AJAX calls.
+
+3.Lossely coupled and extensible
+-We can add new features into exiting application without leading to catastrophic failures.
+
+FAQ: What is Solution?
+Ans-Better build SPA.[Single page Applications]
+
+FAQ: How to build SPA?
+Ans-Java Script, jQuery, Angular, React, Vue, Knockout JS, Ember , Backbone JS
+
+Features Of React JS:
+1.React Uses Virtual DOM.
+   A)What is DOM?
+   Ans-DOM means the real webpage structure shown in the browser.
+       React uses DOM to display content on the screen.
+   B)What is shadow DOM?
+   Ans-Shadow DOM is a hidden DOM that keeps components separate and protected.
+   C)What is virtual DOM?
+   Ans-Virtual DOM is a lightweight copy of the real DOM used by React for faster updates.
+
+2.React of component based
+-Easy to reuse
+-Easy to extend
+-Easy to maintain
+-Easy to test
+-Loosely coupled
+
+3.One Way Data binding
+-Secured Data Binding
+-it will make the field read-only until or unless defended with change.
+
+4.Modular Approach
+-Light weight
+-Faster interactions
